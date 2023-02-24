b7d9f39e469cc4ae373b2b9fd83c10f98c34291e
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-02-24 12:47:21 +0100

    Root file for YelpCamp

---
+++ app.js
@@ -0,0 +1,33 @@
+// Importing node core modules
+const path = require("path");
+
+// Importing npm modules
+const express = require("express");
+require("dotenv").config();
+
+// Importing custom modules
+const MONGO_CONNECTION = require("./database/dbConnection");
+
+// Starting the express app
+const app = express();
+
+// Setting up ejs
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+// app.set("views", "views");
+
+// middlewares
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+
+// Connecting to the server
+const start = async () => {
+  try {
+    MONGO_CONNECTION(process.env.MONGODB_URI);
+    app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
