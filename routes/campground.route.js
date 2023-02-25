a5e364fe0bbedd14e94a19238ee29b8b3292b2af
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-02-25 19:30:45 +0100

    Updated the edit route

--- routes/campground.route.js
+++ routes/campground.route.js
@@ -5,7 +5,11 @@ const campgroundController = require("../controllers/campground.controller");
 
 router.route("/campgrounds").get(campgroundController.getCampgrounds).post(campgroundController.postCampground);
 router.route("/campgrounds/new").get(campgroundController.getNewCampgroundForm);
-router.route("/campground/:id").get(campgroundController.getCampground);
-router.route("/camground/:id/edit").patch(campgroundController.editCampground);
+router
+  .route("/campground/:id")
+  .get(campgroundController.getCampground)
+  .patch(campgroundController.editCampground)
+  .delete(campgroundController.deleteCampground);
+router.route("/camground/:id/edit").get(campgroundController.getEditCampground);
 
 module.exports = router;
