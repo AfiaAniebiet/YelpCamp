1676ef0a63b4af426e1f6fac3e886ed6a03a58b1
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-02-25 19:31:52 +0100

    Added the edit campground controller

--- controllers/campground.controller.js
+++ controllers/campground.controller.js
@@ -25,8 +25,23 @@ const postCampground = async (req, res, next) => {
   res.send(req.body);
 };
 
+const getEditCampground = async (req, res, next) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", {
+    campground,
+  });
+};
+
 const editCampground = async (req, res, next) => {
-  res.send("Edit Campground");
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+  res.redirect(`/api/v1/campgrounds/${campground._id}`);
+};
+
+const deleteCampground = async (req, res, next) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndDelete(id);
+  res.redirect(`/api/v1/campgrounds`);
 };
 
 module.exports = {
@@ -35,4 +50,6 @@ module.exports = {
   postCampground,
   getNewCampgroundForm,
   editCampground,
+  getEditCampground,
+  deleteCampground,
 };
