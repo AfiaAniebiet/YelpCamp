047f441d867fd6df99aad7eec847a836484bcda9
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-02-25 19:26:26 +0100

    Added morgan middleware

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require("path");
 // Importing npm modules
 const express = require("express");
 require("dotenv").config();
+const methodOverride = require("method-override");
+const morgan = require("morgan");
 
 // Importing custom modules
 const MONGO_CONNECTION = require("./database/dbConnection");
@@ -18,8 +20,11 @@ app.set("views", path.join(__dirname, "views"));
 // app.set("views", "views");
 
 // middlewares
+// app.use(morgan("tiny"));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.use("/api/v1", campgroundRoute);
 
