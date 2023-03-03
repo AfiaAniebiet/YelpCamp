f0234a41da553e94bfb10df917ef63cda0c35fec
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-03-03 19:07:37 +0100

    Added Joi authentication to the post route

--- routes/campground.route.js
+++ routes/campground.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const campgroundController = require("../controllers/campground.controller");
+const { joiValidator } = require("../middleware/joi.middleware");
 
 router.route("/campgrounds").get(campgroundController.getCampgrounds).post(campgroundController.postCampground);
 router.route("/campgrounds/new").get(campgroundController.getNewCampgroundForm);
