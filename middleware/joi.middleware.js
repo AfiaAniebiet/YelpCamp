2c084b7f8c2467efcfcd3c8412254bf6477cbac3
AfiaAnieniet <leeboy4u2002@yahoo.com>
2023-03-07 21:19:40 +0100

    Fixed a bug

--- middleware/joi.middleware.js
+++ middleware/joi.middleware.js
@@ -1,15 +1,17 @@
 const Joi = require("joi");
+const { CustomError } = require("../utils/CustomError");
 
-const joiValidator = () => {
-  const campgroundSchema = Joi.object({
-    campground: Joi.object({
-      title: Joi.string().required,
-      price: Joi.number().required().min(0),
-      image: Joi.string().required(),
-      location: Joi.string().required(),
-      description: Joi.string().required(),
-    }).required(),
-  });
+const campgroundSchema = Joi.object({
+  campground: Joi.object({
+    title: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    image: Joi.string().required(),
+    location: Joi.string().required(),
+    description: Joi.string().required(),
+  }).required(),
+});
+
+const validateCampground = (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
   if (error) {
     const msg = error.details.map((el) => el.message).join(",");
@@ -20,5 +22,5 @@ const joiValidator = () => {
 };
 
 module.exports = {
-  joiValidator,
+  validateCampground,
 };
