a830da507ebf9365ca129a1c83335a3bd98fef99
AfiaAnieniet <leeboy4u2002@yahoo.com>
2023-03-07 21:49:11 +0100

    Added validation for reviews

--- middleware/joi.middleware.js
+++ middleware/joi.middleware.js
@@ -21,6 +21,24 @@ const validateCampground = (req, res, next) => {
   }
 };
 
+module.exports.reviewSchema = Joi.object({
+  review: Joi.object({
+    body: Joi.string().required(),
+    rating: Joi.number().required().min(1).max(5),
+  }).required(),
+});
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new CustomError(msg, 400);
+  } else {
+    next();
+  }
+};
+
 module.exports = {
   validateCampground,
+  validateReview,
 };
