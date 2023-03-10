c89c5a2e91928a649891cf13c1cdff2fa35bcc74
AfiaAnieniet <leeboy4u2002@yahoo.com>
2023-03-10 22:21:31 +0100

    Removed review route

--- routes/campground.route.js
+++ routes/campground.route.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const campgroundController = require("../controllers/campground.controller");
-const reviewController = require("../controllers/reviews.controller");
-const { validateCampground, validateReview } = require("../middleware/joi.middleware");
+const { validateCampground } = require("../middleware/joi.middleware");
 
 router
   .route("/campgrounds")
@@ -16,8 +15,5 @@ router
   .patch(campgroundController.editCampground)
   .delete(campgroundController.deleteCampground);
 router.route("/campground/:id/edit").get(campgroundController.getEditCampground);
-
-router.route("/campground/:id/reviews").post(validateReview, reviewController.postReview);
-router.route("/campground/:id/reviews/:reviewId").delete(reviewController.deleteReview);
 
 module.exports = router;
