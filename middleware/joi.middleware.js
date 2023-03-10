b0b4484d5ebda960908218c2f227153fd1e3e3d0
AfiaAnieniet <leeboy4u2002@yahoo.com>
2023-03-10 21:21:13 +0100

    Fixed a bug

--- middleware/joi.middleware.js
+++ middleware/joi.middleware.js
@@ -21,7 +21,7 @@ const validateCampground = (req, res, next) => {
   }
 };
 
-module.exports.reviewSchema = Joi.object({
+const reviewSchema = Joi.object({
   review: Joi.object({
     body: Joi.string().required(),
     rating: Joi.number().required().min(1).max(5),
