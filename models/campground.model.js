f4d1e5d2e64f09d37010b5416710da4d0f104ac5
AfiaAnieniet <leeboy4u2002@yahoo.com>
2023-03-10 21:22:30 +0100

    Added middleware to remove campground with its associated reviews

--- models/campground.model.js
+++ models/campground.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review.model");
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema(
@@ -31,6 +32,17 @@ const CampgroundSchema = new Schema(
   },
   { timestamps: true }
 );
+
+// Middleware to remove a campground and its associated reviews
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+});
 
 const Campground = mongoose.model("Campground", CampgroundSchema);
 module.exports = Campground;
